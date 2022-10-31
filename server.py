6a965604d2342baddb2c65d554b652c88b309a35
Nesim <nessim.jemal@gmail.com>
2022-10-31 16:19:23 -0700

    first commit

--- server.py
+++ server.py
@@ -53,7 +53,8 @@ def handle_robot_cpu_temp_client(conn, addr):
     connected = True
     time.sleep(2)
     while connected:
-        msg= str('{get_cpu_temp_func()}\N{DEGREE SIGN}')
+        # msg= str('{get_cpu_temp_func()}\N{DEGREE SIGN}')
+        msg= str(get_cpu_temp_func())
         conn.send(msg.encode('ASCII'))
         time.sleep(2)
 
